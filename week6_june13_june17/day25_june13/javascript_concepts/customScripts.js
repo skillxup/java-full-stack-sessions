add37ca5d8c673b547d2ae874b38a99830e73848
rkitonlinetrainings <rkitonlinetrainings@gmail.com>
2022-06-13 19:35:55 +0530

    js_localstorage_sessionstorage

---
+++ week6_june13_june17/day25_june13/javascript_concepts/customScripts.js
@@ -0,0 +1,144 @@
+// form validation
+function printError(elemId, errMsg) {
+    document.getElementById(elemId).innerHTML = errMsg;
+}
+
+function validateForm() {
+    // accessing the values from inputs
+    var fullname = document.userForm.fullname.value;
+    var emailaddress = document.userForm.emailaddress.value;
+    var mobilenumber = document.userForm.mobilenumber.value;
+    var country = document.userForm.country.value;
+    var gender = document.userForm.gender.value;
+
+    // finding out the selected values from group of items in checkboxes
+    var hobbies = [];
+    var checkboxes = document.getElementsByName("hobbies[]");
+    for(let i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked) {
+            hobbies.push(checkboxes[i].value);
+        }
+    }
+
+    // defining the error variables with a default value
+    var fullnameErr = emailaddressErr = mobilenumberErr = countryErr = genderErr = true;
+
+    // validate fullname
+    if(fullname == "") {
+        printError("fullnameErr", "please enter your fullname");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;
+        if(regex.test(fullname) === false) {
+            printError("fullnameErr", "please enter a valid fullname");
+        } else {
+            printError("fullnameErr", "");
+            fullnameErr = false;
+        }
+    }
+
+    // validate emailaddress
+    if(emailaddress == "") {
+        printError("emailaddressErr", "please enter your emailaddress");
+    } else {
+        var regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        if(regex.test(emailaddress) === false) {
+            printError("emailaddressErr", "please enter a valid emailaddress");
+        } else {
+            printError("emailaddressErr", "");
+            emailaddressErr = false;
+        }
+    }
+
+    // validate mobilenumber
+    if(mobilenumber == "") {
+        printError("mobilenumberErr", "please enter your mobilenumber");
+    } else {
+        var regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+        if(regex.test(mobilenumber) === false) {
+            printError("mobilenumberErr", "please enter a valid mobilenumber");
+        } else {
+            printError("mobilenumberErr", "");
+            mobilenumberErr = false;
+        }
+    }
+
+    // validate country
+    if(country == "") {
+        printError("countryErr", "please select any country");
+    } else {
+        printError("countryErr", "");
+        countryErr = false;
+    }
+
+    // validate gender
+    if(gender == "") {
+        printError("genderErr", "please select gender");
+    } else {
+        printError("genderErr", "");
+        genderErr = false;
+    }
+
+    // prevent the form being submitted if there are any errors
+    if((fullnameErr || emailaddressErr || mobilenumberErr || countryErr || genderErr) == true) {
+        return false;
+    } else {
+        var dataPreview = `
+            you've have entered the following details: \n 
+            fullname: ${fullname} \n 
+            emailaddress: ${emailaddress} \n 
+            mobilenumber: ${mobilenumber} \n 
+            country: ${country} \n 
+            gender: ${gender} \n 
+        `;
+
+        if(hobbies.length) {
+            dataPreview += `
+                hobbies: ${hobbies.join(', ')}
+            `;
+        }
+
+        alert(dataPreview);
+    }
+}
+
+// session storage
+
+function store() {
+    var brand = document.getElementById("carBrand").value;
+    var price = document.getElementById("carPrice").value;
+
+    const car = {
+        brand: brand, 
+        price: price
+    }
+
+    window.sessionStorage.setItem('car', JSON.stringify(car));
+}
+
+function retreiveRecords() {
+    var records = window.sessionStorage.getItem('car');
+    
+    var paragraph = document.createElement('p');
+    
+    var infor = document.createTextNode(records);
+    paragraph.appendChild(infor);
+
+    var element = document.getElementById('retrieve');
+    element.appendChild(paragraph);
+}
+
+function removeRecord() {
+    sessionStorage.removeItem('car');
+}
+
+function clearAllRecords() {
+    sessionStorage.clear();
+}
+
+window.onload = function() {
+    document.getElementById('carForm').onsubmit = store;
+
+    document.getElementById('clearAllRecords').onclick = clearAllRecords;
+    document.getElementById('removeRecord').onclick = removeRecord;
+    document.getElementById('retreiveRecords').onclick = retreiveRecords;
+}
