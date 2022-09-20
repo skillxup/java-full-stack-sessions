9d1d39eaca347e438f399f1d3ffb3916999d569d
rkitonlinetrainings <rkitonlinetrainings@gmail.com>
2022-09-20 19:33:14 +0530

    Enums

---
+++ week20_sep19_sep23/day87_sep20/EAR/DefineEnumSyntax.java
@@ -0,0 +1,9 @@
+public enum Directions {
+    EAST, 
+    WEST, 
+    NORTH, 
+    SOUTH
+}
+
+// here the Directions is a variable of Enum Type, which is a collection of four constants EAST, WEST, NORTH, SOUTH
+
---
+++ week20_sep19_sep23/day87_sep20/EAR/UseEnumInIfElseStatement.java
@@ -0,0 +1,18 @@
+public enum Directions {
+    EAST, 
+    WEST, 
+    NORTH, 
+    SOUTH
+}
+// here the Directions is a variable of Enum Type, which is a collection of four constants EAST, WEST, NORTH, SOUTH
+
+Directions dir = Directions.NORTH;
+
+if(dir == Directions.EAST) {
+    // do something, write your logic
+} else if(dir == Directions.WEST) {
+    // do something else
+} else {
+    // do domething, write logic for the remaining
+}
+
